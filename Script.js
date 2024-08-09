a0f1dcf23ccf816c7f87be619280dc3831b77830
lipson_44 <arolipson@gmail.com>
2024-08-09 12:36:46 +0530

    my website

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Things to add in this page 
+// 1. form validation and sending mail function
+// 2. change greetings with respect to time on clinet side
+// 3. smooth scolling 
+// 4. maybe add cloud icon based on climate on the client side
+
