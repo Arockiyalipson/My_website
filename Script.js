12e074232a63f4c860a54eec3f5a09e04f23fbee
lipson_44 <arolipson@gmail.com>
2024-08-09 22:56:20 +0530

    Added greetings change for client side

--- Script.js
+++ Script.js
@@ -1,6 +1,23 @@
 // Things to add in this page 
-// 1. form validation and sending mail function
-// 2. change greetings with respect to time on clinet side
 // 3. smooth scolling 
 // 4. maybe add cloud icon based on climate on the client side
 
+
+// change in greetings
+let greetings = document.getElementById('intro')
+console.log(greetings)
+//greetings change based on time
+    const time = new Date().getHours();
+    if(time>= 6 && time <12){
+        greetings.textContent = "Good Morning !!!!"
+    }
+    else if(time >=12 && time <18){ 
+    greetings.textContent = "Good afternoon !!!"
+    }
+    else if(time >=18) {
+        greetings.textContent = "Good evening !!"
+    }
+    else{
+        console.log("error in changing ")
+    }
+    console.log(time)
